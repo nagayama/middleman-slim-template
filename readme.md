39c9d8834f7d1ad0f608672049f782a4492bf885
Max Luster <maxluster@gmail.com>
2013-10-09 14:15:26 -0700

    Chart scales on resize

--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
 # Middleman 3.0.x Project Template: HTML5 Boilerplate, Slim templates, Normalize
+
+1. bundle install
+2. bundle exec middleman
--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -1,75 +1,50 @@
-var aspectRatio = 1 / 2
+var bs = {};
 
-// Fill container wdith
-containerWidth = $("#vis").width();
-containerHeight = containerWidth * aspectRatio;
+bs.stackedArea = function(el, data){
 
+  // Constants
+  var aspectRatio = 1 / 2,
+      container = d3.select(el.get(0));
 
-// Init SVG with margin convention
-// TODO: Explore methods for updating margins relatively in repaint cycle
-var margin = {top: 0, right: 0, bottom: 36, left: 0},  
-  width = containerWidth - margin.left - margin.right,
-  height = containerHeight - margin.top - margin.bottom;
+  
+  // Axis generators
+  var formatX = d3.time.format("%Y"),
+      formatY = d3.format();
 
-var vis = d3.select("#vis");
+  var xAxis = d3.svg.axis()
+    .orient("bottom")
+    .tickFormat(formatX)
+    .tickPadding("10");
 
-var header = vis.append("h6").classed("title", true)
-    .text("Chart Title Placeholder");
+  var yAxis = d3.svg.axis()
+    .orient("left")
+    .tickFormat(formatY)
+    .ticks(6) // TODO: Get smart about number of ticks  
 
-var legend = vis.append("ul").classed("legend", true);
+  
+  // Init mark generators
+  var area = d3.svg.area()  // Area generator accesses y and y0 properties attached from stack generator
+    .x(function(d) { return x(new Date(d[0]) ); 
+    })
+    .y0(function(d) { return y(d.y0); })
+    .y1(function(d) { return y(d.y0 + d.y) });
 
-var svg = vis.append("svg").classed("chart", true)
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    // .attr("preserveAspectRatio", "xMinYMin meet")
-    // .attr("viewBox", "0 0 " + (width + margin.left + margin.right) + " " + (height + margin.top + margin.bottom))
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  var stack = d3.layout.stack()  // Stack layout attaches y and y0 properties to value pairs
+    .values(function(d) { return d.values; })
+    .offset("zero")
+    .x(function(d) { return d[0] } )
+    .y(function(d) { return d[1] } )
 
+  stack(data); // Attach y, y0 to value pairs
 
-// Scales
-var x = d3.time.scale().range([0, width])
-    y = d3.scale.linear().range([height, 0])
-    color = d3.scale.category20();
+  
+  // Init scales
+  var x = d3.time.scale()
+      y = d3.scale.linear()
+      color = d3.scale.category20();
 
-// Axis generators
-var formatX = d3.time.format("%Y"),
-    formatY = d3.format();
-
-var xAxis = d3.svg.axis()
-  .scale(x)
-  .orient("bottom")
-  .tickFormat(formatX)
-  .tickPadding("10");
-
-var yAxis = d3.svg.axis()
-  .scale(y)
-  .orient("left")
-  .tickFormat(formatY)
-  .ticks(6) // TODO: Get smart about number of ticks
-  .innerTickSize(-width);
-
-// Mark generators
-var area = d3.svg.area()  // Area generator accesses y and y0 properties attached from stack generator
-  .x(function(d) { return x(new Date(d[0]) ); 
-  })
-  .y0(function(d) { return y(d.y0); })
-  .y1(function(d) { return y(d.y0 + d.y) });
-
-var stack = d3.layout.stack()  // Stack layout attaches y and y0 properties to each values pair array
-  .values(function(d) { return d.values; })
-  .offset("zero")
-  .x(function(d) { return d[0] } )
-  .y(function(d) { return d[1] } )
-
-
-// Render
-d3.json("datasets/stackedAreaData.json", function(error, data) {
-
-  // Stack layout attaches y and y0 properties to value pair arrays
-  stack(data);
-
-  // Set Domains
+  
+  // Calculate domains
   var yMax;
   data.forEach(function(d){
     d.values.forEach(function(d){
@@ -79,7 +54,7 @@ d3.json("datasets/stackedAreaData.json", function(error, data) {
     });
   });
   y.domain([0,yMax]);
-   
+  
   x.domain(d3.extent(data[0].values, function(d) { return new Date(d[0]); }));
 
   var names = [];
@@ -88,41 +63,34 @@ d3.json("datasets/stackedAreaData.json", function(error, data) {
   });
   color.domain(names);
 
+  
+  // DOM Scaffolding 
+  var header = container.append("h6").classed("title", true)
+      .text("Chart Title Placeholder");
 
-  // Draw Axes
-  gx = svg.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")")
-    .call(xAxis);
+  var legend = container.append("ul").classed("legend", true);
 
-  gx.selectAll("text")
-    .attr("dx", ".25em")
-    .style("text-anchor", "end");
+  var svg = container.append("svg").classed("chart", true);
+    
+  var canvas = svg.append("g")
+    .attr("class", "canvas");
 
-  gy = svg.append("g")
-    .attr("class", "y axis")
-    .call(yAxis); 
+  var gxAxis = canvas.append("g")
+    .attr("class", "x axis");
 
-  gy.selectAll("text")
-    .attr("dx", "1em")
-    .attr("dy", "1.25em")
-    .style("text-anchor", "start");
+  var gyAxis = canvas.append("g")
+    .attr("class", "y axis");
 
-
-  // Draw areas
-  var series = svg.selectAll(".series")
+  var series = canvas.selectAll(".series")
     .data(data)
     .enter().append("g")
     .attr("class", "series");
 
-  series.append("path")
-    .attr("class", "area")
-    .attr("d", function(d) { 
-      return area(d.values); 
-    })
-    .style("fill", function(d) { return color(d.name); });
+  var areas = series.append("path")
+    .attr("class", "area");
 
-  // Draw legend
+
+  // Render legend
   var legendListItems = legend.selectAll("li")
     .data(data)
     .enter()
@@ -135,6 +103,79 @@ d3.json("datasets/stackedAreaData.json", function(error, data) {
 
   legendListItems.append("span").classed("text", true).text(function(d){
     return d.name;
+  });
+
+  var render = function(){
+
+    console.time("render");
+
+    // Calculate current dimensions
+    var containerWidth = el.width(),
+        containerHeight = containerWidth * aspectRatio,
+        margin = {top: 0, right: 0, bottom: 36, left: 0},  // TODO: set margins in EM space
+        width = containerWidth - margin.left - margin.right,
+        height = containerHeight - margin.top - margin.bottom;
+
+    
+    // Update ranges and axis generators
+    x.range([0, width]);
+    y.range([height, 0]);
+    xAxis.scale(x);
+    yAxis
+      .scale(y)
+      .innerTickSize(-width);
+
+
+    // Draw Axes
+    gxAxis
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+    gxAxis.selectAll("text")
+      .attr("dx", ".25em")
+      .style("text-anchor", "end");      
+
+    gyAxis
+      .call(yAxis); 
+
+    gyAxis.selectAll("text")
+      .attr("dx", "1em")
+      .attr("dy", "1.25em")
+      .style("text-anchor", "start");  
+
+
+    // Update cbart   TODO: separate marks layer from axis layer. Scale marks with viewbox, axis with js
+    svg
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      // .attr("preserveAspectRatio", "xMinYMin meet")
+      // .attr("viewBox", "0 0 " + (width + margin.left + margin.right) + " " + (height + margin.top + margin.bottom))
+    
+    canvas
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    areas
+      .attr("d", function(d) { 
+        return area(d.values); 
+      })
+      .style("fill", function(d) { return color(d.name); });
+
+    console.timeEnd("render");
+
+  }
+
+  // Render and attach resize handler   TODO: debounce
+  render.call();
+  d3.select(window).on("resize", render);
+
+};
+
+
+
+$(function(){
+
+  d3.json("datasets/stackedAreaData.json", function(error, data) {
+    var vis = bs.stackedArea($("#vis"), data);
   });
 
 });
